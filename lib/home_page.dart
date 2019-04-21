63eded37e17c86d69b511fef1a402da92c236ca5
Korakot Leemakdej <kleemakdej@gmail.com>
2019-04-21 14:11:33 -0700

    layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,44 +24,62 @@ class _HomePageState extends State<HomePage> {
     {"open": 50.0, "high": 100.0, "low": 40.0, "close": 80, "volumeto": 5000.0},
     {"open": 80.0, "high": 90.0, "low": 55.0, "close": 65, "volumeto": 4000.0},
     {"open": 65.0, "high": 120.0, "low": 60.0, "close": 90, "volumeto": 7000.0},
-    {"open": 90.0, "high": 95.0, "low": 85.0, "close": 80, "volumeto": 2000.0},
-    {"open": 80.0, "high": 85.0, "low": 40.0, "close": 50, "volumeto": 3000.0},
+    {"open": 90.0, "high": 95.0, "low": 70.0, "close": 80, "volumeto": 2000.0},
+    {"open": 70.0, "high": 100.0, "low": 50.0, "close": 50, "volumeto": 3000.0},
+    {"open": 70.0, "high": 100.0, "low": 50.0, "close": 50, "volumeto": 3000.0},
   ];
+  int i = 3;
   @override
   Widget build(BuildContext context) {
-    config();
+    setLandscape();
     return new Scaffold(
         body: SafeArea(
             child: Stack(
       children: <Widget>[
-        _text("KorStock", "Megrim"),
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: OHLCVGraph(
-                  data: sampleData, enableGridLines: true, volumeProp: 0.2),
-            ),
-            Container(
-              width: 100.0,
-            )
-          ],
+        bg("KorStock $i", "Megrim"),
+        candle(),
+        Positioned(
+          right: 20.0,
+          top: 5.0,
+          child: Text('coins: 100',
+              style: TextStyle(
+                  fontSize: 18, fontFamily: "Bitter", color: Colors.white)),
         ),
-        // _text("KorStock", "Bitter")
       ],
     )));
   }
 
-  Widget _text(text, font) {
+  Widget candle() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0, bottom: 10.0, left: 10.0),
+            child: OHLCVGraph(
+                increaseColor: Colors.greenAccent,
+                decreaseColor: Colors.redAccent,
+                data: sampleData,
+                enableGridLines: true,
+                volumeProp: 0.2),
+          ),
+        ),
+        Container(
+          width: 100.0,
+        )
+      ],
+    );
+  }
+
+  Widget bg(text, font) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Container(
         width: width,
         height: height - 25,
         // height: (height - 25) / 3,
-        child:
-            Text(text, style: TextStyle(
-              fontSize: 24,
-              fontFamily: font, color: Colors.white)),
+        child: Text(text,
+            style:
+                TextStyle(fontSize: 24, fontFamily: font, color: Colors.white)),
         // child: FittedBox(
         //   fit: BoxFit.contain,
         //   child: Text(text,
@@ -73,14 +91,14 @@ class _HomePageState extends State<HomePage> {
             end: Alignment.bottomCenter,
             stops: [0.1, 0.9],
             colors: [
-              Colors.indigo[200],
-              Colors.greenAccent[200],
+              Colors.brown,
+              Colors.amberAccent,
             ],
           ),
         ));
   }
 
-  void config() {
+  void setLandscape() {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
   }
